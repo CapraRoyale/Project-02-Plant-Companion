ab7cb0b61aa3410e69626e6c00992ba7e5115b44
CapraRoyale <siege.lehman@gmail.com>
2019-10-18 14:07:04 -0700

    Finish creating migration files and db relations Use Sequelize CLI to instantiate models and relations via model:generate and db:migrate

--- db/models/bad_comp.js
+++ db/models/bad_comp.js
@@ -1,12 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const bad_comp = sequelize.define('bad_comp', {
-    hinderer: DataTypes.INTEGER,
-    hinderee: DataTypes.INTEGER,
-    desc: DataTypes.STRING
-  }, {});
-  bad_comp.associate = function(models) {
-    // associations can be defined here
-  };
-  return bad_comp;
+    const bad_comp = sequelize.define('bad_comp', {
+        hinderer: DataTypes.INTEGER,
+        hinderee: DataTypes.INTEGER,
+        desc: DataTypes.STRING
+    }, {});
+    bad_comp.associate = function(models) {
+        // associations can be defined here
+        bad_comp.belongsTo(models.plant);
+    };
+    return bad_comp;
 };
--- db/models/good_comp.js
+++ db/models/good_comp.js
@@ -1,12 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const good_comp = sequelize.define('good_comp', {
-    helper: DataTypes.INTEGER,
-    helpee: DataTypes.INTEGER,
-    desc: DataTypes.STRING
-  }, {});
-  good_comp.associate = function(models) {
-    // associations can be defined here
-  };
-  return good_comp;
+    const good_comp = sequelize.define('good_comp', {
+        helper: DataTypes.INTEGER,
+        helpee: DataTypes.INTEGER,
+        desc: DataTypes.STRING
+    }, {});
+    good_comp.associate = function(models) {
+        // associations can be defined here
+        bad_comp.belongsTo(models.plant);
+    };
+    return good_comp;
 };
