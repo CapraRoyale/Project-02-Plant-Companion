5a16759c1e44bbf2ff0de095683cf99a15a0b93c
CapraRoyale <siege.lehman@gmail.com>
2019-10-18 18:43:08 -0700

    Delete duplicate index file and add future seeder

--- db/seeders/20191018233006-new-plants.js
+++ db/seeders/20191018233006-new-plants.js
@@ -1,26 +1,44 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert('plants', [{
+                name: 'Apple',
+                desc: 'the round fruit of a tree of the rose family, which typically has thin red or green skin and crisp flesh',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                name: 'Banana',
+                desc: 'a long curved fruit which grows in clusters and has soft pulpy flesh and yellow skin when ripe',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                name: 'Carrot',
+                desc: 'a tapering orange-colored root eaten as a vegetable',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            }, {
+                name: 'Dragonfruit',
+                desc: 'A night-blooming climbing cactus (Hylocereus undatus) native to Mexico and Central America, cultivated in tropical regions for its edible fruit',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            }, {
+                name: 'Eggplant',
+                desc: 'the purple egg-shaped fruit of the nightshade family, which is eaten as a vegetable.',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            }, {
+                name: 'Fig',
+                desc: 'a soft pear-shaped fruit with sweet dark flesh and many small seeds, eaten fresh or dried',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+        ], {});
+    },
 
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
-  },
-
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
-  }
-};
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.bulkDelete('plants', [{}]);
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Plant Companion' });
+    // res.render('index', { title: 'Plant Companion' });
 });
 
 module.exports = router;
