d9e5e22b8854284c6df468d1c72fbc8659783698
CapraRoyale <siege.lehman@gmail.com>
2019-11-15 08:25:44 -0800

    ADD edit hbs file, add edit route

--- routes/plants.js
+++ routes/plants.js
@@ -2,13 +2,14 @@ var express = require('express');
 var router = express.Router();
 var db = require("../db/models")
 
-/* GET users listing. */
+/* GET all plant entries listing. */
 router.get('/', function(req, res, next) {
     db.plant.findAll()
         .then(pl => res.render('plants', {
             plant: pl
         }));
 });
+// GET a single plant entry
 router.get('/:id', function(req, res, next) {
     db.plant.findOne({
             where: {
@@ -19,6 +20,21 @@ router.get('/:id', function(req, res, next) {
             console.log([pl]);
             res.render('plants', {
                 plant: [pl]
+            })
+        });
+});
+// GET a single plant entry to edit
+router.get('/:id/edit', function (req, res, next) {
+    db.plant.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(pl => {
+            console.log([pl]);
+            res.render('plants', {
+                plant: [pl],
+                edit: true
             })
         });
 });
